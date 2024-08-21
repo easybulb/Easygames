db7f084e63224b2af6ddb38cab8d4e5a36ce6972
Henry Ofodieze <easybulb@gmail.com>
2024-08-21 02:38:15 +0000

    Add function to position fly ramdomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,16 @@ let fly = document.getElementById('fly');
 let scoreBoard = document.getElementById('score-board')
 let gameContainer = document.getElementById('game-container')
 
-// calculates a random position for the fly.
+// calculates a random position for fly.
 function getRandomPosition() {
     const x = Math.floor(Math.random() * (gameContainer.clientWidth - fly.clientWidth));
     const y = Math.floor(Math.random() * (gameContainer.clientHeight - fly.clientHeight));
     return { x, y };
+}
+
+// Position fly at random location
+function moveFly() {
+    const { x, y } = getRandomPosition();
+    fly.style.left = `${x}px`;
+    fly.style.top = `${y}px`;
 }
