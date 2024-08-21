c2351955ce41bc1757709c0b140bd34e7f146ad2
Henry Ofodieze <easybulb@gmail.com>
2024-08-21 02:35:53 +0000

    Add function to calculate random position for the fly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ let score = 0;
 let fly = document.getElementById('fly');
 let scoreBoard = document.getElementById('score-board')
 let gameContainer = document.getElementById('game-container')
+
+// calculates a random position for the fly.
+function getRandomPosition() {
+    const x = Math.floor(Math.random() * (gameContainer.clientWidth - fly.clientWidth));
+    const y = Math.floor(Math.random() * (gameContainer.clientHeight - fly.clientHeight));
+    return { x, y };
+}
