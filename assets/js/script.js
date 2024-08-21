b69fd3313100feabb8cf4a91f7abbe143c0089bd
Henry Ofodieze <easybulb@gmail.com>
2024-08-21 03:00:07 +0000

    Added html DOM access

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 let score = 0;
+let countdown;
+let timeLeft;
 
 // Access game html elements
 let fly = document.getElementById('fly');
 let scoreBoard = document.getElementById('score-board')
 let gameContainer = document.getElementById('game-container')
+const timer = document.getElementById('timer');
+const startScreen = document.getElementById('start-screen');
+const endScreen = document.getElementById('end-screen');
+const startButton = document.getElementById('start-button');
+const restartButton = document.getElementById('restart-button');
+const finalScore = document.getElementById('final-score');
+const timeSelect = document.getElementById('time-select');
 
 // calculates a random position for fly.
 function getRandomPosition() {
