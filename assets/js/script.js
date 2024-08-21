759bb75b28a2aae05df137cecd701b3b03fa7a5a
Henry Ofodieze <easybulb@gmail.com>
2024-08-21 02:33:33 +0000

    Initial JS code - Add DOM element access

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let score = 0;
+
+// Access game html elements
+let fly = document.getElementById('fly');
+let scoreBoard = document.getElementById('score-board')
+let gameContainer = document.getElementById('game-container')
